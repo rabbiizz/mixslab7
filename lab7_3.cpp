d2388d9e6c9d42bb0e3bad127b1fdb0498b27d56
rabbiizz <Supakarn_k@cmu.ac.th>
2023-12-08 16:52:37 +0700

    lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,20 @@
-#include<iostream>
+//ไม่ต้องเติม function main()
+#include <iostream>
+#include <string>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    char ans;
+    if(x == 'A'){
+    ans = 'Z';
+    return ans;
+    }
+    if(x < 'A' || x > 'Z'){
+    ans = '0';
+    return ans;
+    }else{
+    ans = x-1;
+    return ans;
+    }
+    
 }
